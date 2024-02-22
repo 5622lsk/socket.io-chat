870150820a3179baa535a7c4b426239248a607f9
임성경 <5622lsk@naver.com>
2024-02-22 18:07:48 +0900

    feat : 채팅 리스트 생성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,27 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 //여기서의 socket은 서버로의 연결을 뜻함.
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server");
-});
+function handleOpen(){
+    console.log("Connected to Server");    
+}
+socket.addEventListener("open", handleOpen);
 
 socket.addEventListener("message", (message) => {
-    console.log("New message:", message.data);
+    const li = document.createElement("li"); //메시지를 받으면 새로운 li생성
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server ❌");
-})
+});
 
-setTimeout(() => {
-    socket.send("하이 from the Browser");
-},10000);
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+messageForm.addEventListener("submit", handleSubmit); 
--- src/server.js
+++ src/server.js
@@ -15,14 +15,25 @@ const handleListen = () => console.log( `Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
+function onSocketClose(){
+    console.log("Disconnected from the Browser");
+}
+function onSocketMessage(message){
+    console.log(message);
+} 
+
+const sockets = []; //연결된 브라우저를 넣는 데이터베이스
+
 wss.on("connection",(socket) => {  //여기서의 socket은 연결된 브라우저를 뜻함.
+    sockets.push(socket); 
     console.log("Connected to Browser");
-    socket.on("close", () => console.log("Disconnected from the Browser") );
+    socket.on("close", onSocketClose);
     socket.on("message", (message) => {
-        console.log(message);
+        const messageString = message.toString('utf8');
+        socket.send(messageString);
+        sockets.forEach((aSocket) => aSocket.send(message));
     });
-    socket.send("hello!!??");  //socket이 프론트와 실시간으로 소통할 수 있음
 });
+  //socket이 프론트와 실시간으로 소통할 수 있음
 
 server.listen(3000, handleListen);
-// app.listen(3000,handleListen);
